9630665dc4aae0ff1be99acc75b0822aa27a66ab
abhay8696 <kambleabhaygautam@gmail.com>
2024-09-25 23:53:41 +0530

    updated app.js to catch all routes to serve the index.html for any unhandled routes(params by react router)

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const { errorHandler } = require("./middlewares/error");
 const routes = require("./routes/v1");
 const ApiError = require("./utils/ApiError");
+const path = require("path");
 
 const app = express();
 
@@ -25,7 +26,13 @@ app.use(compression());
 app.use(cors());
 app.options("*", cors());
 
+app.use(express.static(path.join(__dirname, "build"))); // Serve static files - //catch all routes to serve the index.html for any unhandled routes(params by react router)
+
 app.use("/v1", routes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html")); //catch all routes to serve the index.html for any unhandled routes(params by react router)
+});
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
